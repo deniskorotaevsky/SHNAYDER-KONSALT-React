a3b5c6d7dea1a48644110ab53b270319c4fc3ffa
Denis Korotaevsky <huligan8771@gmail.com>
2022-11-05 04:17:40 +0300

    @media

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 let format = 'DD.MM.YYYY';
 let m = moment();
-document.querySelector('.date_week').innerHTML = m.startOf('week').add(8, 'days').format(format);
-
+document.querySelector('.js-sale').innerHTML = m.startOf('week').add(8, 'days').format(format);
